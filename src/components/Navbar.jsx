62b1d13105cce0df6624752dfc39a45a73367448
Vidhi Patel <vidhilotus7@gmail.com>
2026-02-24 15:44:11 +0530

    Nav ready with dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,88 +16,42 @@ function Navbar() {
     setTheme(storedTheme);
     document.documentElement.classList.toggle("dark", storedTheme === "dark");
   }, []);
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/skills", label: "Skills" },
+    { path: "/projects", label: "Projects" },
+    { path: "/contact", label: "Contact" },
+  ];
 
   return (
     <header className="shadow sticky z-50 top-0">
       <nav className="dark:bg-bg-dark bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex justify-between items-center mx-auto max-w-screen-2xl">
+          {/* Logo */}
           <h1 className="flex items-center text-3xl font-semibold text-violet">
             Vidhi Patel
           </h1>
-
           {/* Desktop Nav */}
           <div className="hidden md:flex justify-between w-full md:w-auto md:order-1">
             <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0 text-violet">
-              <li>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer ${
-                      isActive
-                        ? "text-text-muted border-b-2 border-violet"
-                        : "text-violet md:border-transparent"
-                    }`
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer ${
-                      isActive
-                        ? "text-text-muted border-b-2 border-violet"
-                        : "text-violet md:border-transparent"
-                    }`
-                  }
-                >
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/skills"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer ${
-                      isActive
-                        ? "text-text-muted border-b-2 border-violet"
-                        : "text-violet md:border-transparent"
-                    }`
-                  }
-                >
-                  Skills
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/projects"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer ${
-                      isActive
-                        ? "text-text-muted border-b-2 border-violet"
-                        : "text-violet md:border-transparent"
-                    }`
-                  }
-                >
-                  Projects
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/contact"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer ${
-                      isActive
-                        ? "text-text-muted border-b-2 border-violet"
-                        : "text-violet md:border-transparent"
-                    }`
-                  }
-                >
-                  Contact
-                </NavLink>
-              </li>
+              {navItems.map(({ path, label }) => (
+                <li key={path}>
+                  <NavLink
+                    to={path}
+                    className={({ isActive }) =>
+                      `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-violet lg:hover:bg-transparent  cursor-pointer 
+                      ${
+                        isActive
+                          ? "text-text-muted border-b-2 border-violet"
+                          : "text-violet md:border-transparent"
+                      }`
+                    }
+                  >
+                    {label}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
           {/* Mobile Nav */}
@@ -107,68 +61,23 @@ function Navbar() {
             } md:hidden justify-between w-full`}
           >
             <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0 text-violet">
-              <li>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent cursor-pointer ${
-                      isActive ? "text-text-muted" : "text-violet"
-                    }`
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/about"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent lg:hover:bg-transparent  cursor-pointer ${
-                      isActive ? "text-text-muted" : "text-violet"
-                    }`
-                  }
-                >
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/skills"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent lg:hover:bg-transparent  cursor-pointer ${
-                      isActive ? "text-text-muted" : "text-violet "
-                    }`
-                  }
-                >
-                  Skills
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/projects"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent lg:hover:bg-transparent  cursor-pointer ${
-                      isActive ? "text-text-muted" : "text-violet"
-                    }`
-                  }
-                >
-                  Projects
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/contact"
-                  className={({ isActive }) =>
-                    `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent lg:hover:bg-transparent  cursor-pointer ${
-                      isActive ? "text-text-muted" : "text-violet"
-                    }`
-                  }
-                >
-                  Contact
-                </NavLink>
-              </li>
+              {navItems.map(({ path, label }) => (
+                <li key={path}>
+                  <NavLink
+                    to={path}
+                    onClick={() => setOpen(false)}
+                    className={({ isActive }) =>
+                      `hover:text-text-muted block py-2 pr-4 pl-3 border-b border-transparent cursor-pointer 
+                    ${isActive ? "text-text-muted" : "text-violet"}`
+                    }
+                  >
+                    {label}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
+          {/* dark mode toggle and hamburger */}
           <div className="flex items-center gap-3 md:order-2">
             <button
               onClick={toggleTheme}
