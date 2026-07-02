04205201985966d62a22fae4ce7a6221eb111f3d
Vidhi Patel <vidhilotus7@gmail.com>
2026-07-02 20:23:24 +0530

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
+    <Analytics />
   </StrictMode>,
 )
 
